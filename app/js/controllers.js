84174b7fd31f115a7a8700cd1ee9cc50233b6ecd
lwschjang <webmaster.lws@gmail.com>
2014-11-06 23:26:33 +0000

    added items json file

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,9 @@
 var allNaturalApp = angular.module('allNaturalApp', []);
 
-allNaturalApp.controller('ItemListCtrl', function ($scope) {
-  $scope.items = [
-    {'name': 'Windex',
-     'snippet': 'Sprays stuff',
- 	 'age': 1},
-    {'name': 'Mr. Clean',
-     'snippet': 'Cleans floors.',
- 	 'age': 2},
-    {'name': 'All-Purpose Cleaner',
-     'snippet': 'Cleans stuffs',
- 	 'age': 3}
-  ];
-
+allNaturalApp.controller('ItemListCtrl', function ($scope, $http) {
+  $http.get('items/items.json').success(function(data) {
+    $scope.items = data;
+  });
 
   $scope.orderProp = 'age';
 });
