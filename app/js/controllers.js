5bef2ab5dae4ee66cb614f002a09d808e95fa29a
lwschjang <webmaster.lws@gmail.com>
2014-11-06 22:53:08 +0000

    added sorting and binding

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,16 @@ var allNaturalApp = angular.module('allNaturalApp', []);
 allNaturalApp.controller('ItemListCtrl', function ($scope) {
   $scope.items = [
     {'name': 'Windex',
-     'snippet': 'Sprays stuff'},
+     'snippet': 'Sprays stuff',
+ 	 'age': 1},
     {'name': 'Mr. Clean',
-     'snippet': 'Cleans floors.'},
+     'snippet': 'Cleans floors.',
+ 	 'age': 2},
     {'name': 'All-Purpose Cleaner',
-     'snippet': 'Cleans stuffs'}
+     'snippet': 'Cleans stuffs',
+ 	 'age': 3}
   ];
+
+
+  $scope.orderProp = 'age';
 });
